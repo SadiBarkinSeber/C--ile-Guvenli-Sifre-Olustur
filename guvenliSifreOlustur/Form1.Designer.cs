4cacd149f67811b5a09e55e630194fc1ec42ff42
Sadi Barkın Şeber <sadibarkn@gmail.com>
2023-09-28 02:19:00 +0300

    Güvenli Sifre Olustur

---
+++ guvenliSifreOlustur/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace guvenliSifreOlustur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSifre2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.BackColor = System.Drawing.Color.Transparent;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.ForeColor = System.Drawing.Color.White;
+            this.lblSonuc.Location = new System.Drawing.Point(171, 168);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 24);
+            this.lblSonuc.TabIndex = 0;
+            // 
+            // txtSifre2
+            // 
+            this.txtSifre2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre2.Location = new System.Drawing.Point(175, 123);
+            this.txtSifre2.Name = "txtSifre2";
+            this.txtSifre2.Size = new System.Drawing.Size(430, 27);
+            this.txtSifre2.TabIndex = 1;
+            this.txtSifre2.TextChanged += new System.EventHandler(this.txtSifre2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(258, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GÜVENLİ ŞİFRE UYGULAMASI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::odev6.Properties.Resources.password;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(777, 272);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSifre2);
+            this.Controls.Add(this.lblSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtSifre2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ guvenliSifreOlustur/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guvenliSifreOlustur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSifre2_TextChanged(object sender, EventArgs e)
+        {
+            string password = txtSifre2.Text;
+
+            if (password.Length > 6)
+            {
+                // Şifre hem özel karakterleri hem de sayıları içeriyorsa yüksek güvenlikli kabul edelim
+                if (password.Any(char.IsDigit) && password.Any(IsSpecialCharacter))
+                {
+                    lblSonuc.Text = "YÜKSEK";
+                    lblSonuc.ForeColor = Color.ForestGreen;
+                }
+                else
+                {
+                    lblSonuc.Text = "ORTA";
+                    lblSonuc.ForeColor = Color.Yellow;
+                }
+            }
+            else
+            {
+                lblSonuc.Text = "DÜŞÜK";
+                lblSonuc.ForeColor = Color.Red;
+            }
+        }
+
+        private bool IsSpecialCharacter(char c)
+        {
+            char[] specialCharacters = { '!', '@', '#', '$', '%', '^', '&', '*' };
+            return specialCharacters.Contains(c);
+        }
+    }
+}
